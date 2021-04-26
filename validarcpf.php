8c7e035fe71c1e4994ec652bb15b1388e6b2907d
Cireid <gabrieldieric@gmail.com>
2021-04-26 14:48:20 -0300

    alteração de padrão

--- validarcpf.php
+++ validarcpf.php
@@ -15,18 +15,21 @@
     {
         $cpf = substr($cpf, 0, 9);
 
-        for ($i = 0; $i < 9; $i++) {
+        for ($i = 0; $i < 9; $i++)
+        {
             $soma_digitos = $soma_digitos + ($cpf[$i] * $posicoes);
             $posicoes--;
         }
 
-        if ($soma_digitos % 11 < 2) {
+        if ($soma_digitos % 11 < 2)
+        {
             $soma_digitos = 0;
             $cpf = $cpf.$soma_digitos;
             return  $cpf;
         };
 
-        if ($soma_digitos % 11 >= 2) {
+        if ($soma_digitos % 11 >= 2)
+        {
             $soma_digitos = 11 - ($soma_digitos % 11);
             $cpf = $cpf.$soma_digitos;
             return  $cpf;
@@ -37,18 +40,21 @@
     function cpf_segundo_digito($cpf, $soma_digitos = 0, $posicoes = 11) {
         $novo_cpf = cpf_primeiro_digito($cpf);
 
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < 10; $i++)
+        {
             $soma_digitos = $soma_digitos + ($novo_cpf[$i] * $posicoes);
             $posicoes--;
         }
 
-        if($soma_digitos % 11 < 2) {
+        if($soma_digitos % 11 < 2)
+        {
             $soma_digitos = 0;
             $novo_cpf = $novo_cpf . $soma_digitos;
             return$novo_cpf;
         }
 
-        if($soma_digitos % 11 >= 2) {
+        if($soma_digitos % 11 >= 2)
+        {
             $soma_digitos = 11 - ($soma_digitos % 11);
             $novo_cpf = $novo_cpf . $soma_digitos;
             return $novo_cpf;
@@ -56,8 +62,10 @@
         return $novo_cpf;
     }
 
-    if(isset($_POST['cpf'])){
-        if($_POST['cpf'] != cpf_segundo_digito($_POST['cpf'])){
+    if(isset($_POST['cpf']))
+    {
+        if($_POST['cpf'] != cpf_segundo_digito($_POST['cpf']))
+        {
             echo "CPF INVALIDO";
             return;
         }
